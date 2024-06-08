a0f5add062d8f60dbb3f5828d7ca1fcb78fd1cd9
gaiijinn <vladruban8@gmail.com>
2024-06-09 02:33:48 +0300

    aggregate and annotate + just small cache

--- learn_service/service_apps/services/serializers.py
+++ learn_service/service_apps/services/serializers.py
@@ -19,9 +19,15 @@ class SubscriptionSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.company_name')
     client_email = serializers.EmailField(source='client.user.email')
     service_name = serializers.CharField(source='service.name')
+    service_raw_price = serializers.CharField(source='service.full_price')
+    final_price = serializers.SerializerMethodField()
 
     plan = PlanSerializer()
 
+    def get_final_price(self, instance):
+        return instance.final_price
+
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'client_email', 'service_name', 'plan')
+        fields = ('id', 'plan_id', 'client_name', 'client_email', 'service_name', 'service_raw_price', 'plan',
+                  'final_price')
--- learn_service/service_apps/services/views.py
+++ learn_service/service_apps/services/views.py
@@ -1,18 +1,30 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 from .models import Subscription, Client
 from .serializers import SubscriptionSerializer
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 
 # Create your views here.
 
 
 class SubscriptionReadOnlyViewSet(ReadOnlyModelViewSet):
-    #queryset = Subscription.objects.all().select_related('').only()
     queryset = Subscription.objects.all().select_related('service', 'plan').prefetch_related(
         Prefetch('client',
                  queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')
                  ),
-    )
+    ).annotate(final_price=(F('service__full_price') -
+                            F('service__full_price') *
+                            F('plan__discount_percent') / 100.00))
+
     serializer_class = SubscriptionSerializer
+
+    @method_decorator(cache_page(60 * 2))
+    def list(self, request, *args, **kwargs):
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {'result': response.data, 'total_sum': self.get_queryset().aggregate(total=Sum('final_price'))}
+        response.data = response_data  # делаем вложеность
+
+        return response
